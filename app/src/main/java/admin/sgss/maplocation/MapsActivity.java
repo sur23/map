7c071859cbc99c9dc12d9d50734a0648f91aeb68
sur23 <40954947+sur23@users.noreply.github.com>
2018-07-09 04:04:56 +0530

    Update MapsActivity.java

--- app/src/main/java/admin/sgss/maplocation/MapsActivity.java
+++ app/src/main/java/admin/sgss/maplocation/MapsActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.ActivityCompat;
@@ -27,7 +28,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -52,10 +63,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     double current_longitude=0.0;
     double current_latitude=0.0;
     String title=null;
+    ArrayList<LatLng> MarkerPoints1;
+    ArrayList<LatLng> MarkerPoints2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        // Initializing
+        MarkerPoints1 = new ArrayList<>();
+        // Initializing
+        MarkerPoints2 = new ArrayList<>();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -81,12 +98,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Show the Mumbai Airport location on the map.
-        showMumbaiAirportLocation();
-
-        // Show the Chennai Airport location on the map.
-        showChennaiAirportLocation();
 
         // Turn on the My Location layer and the related control on the map.
         updateLocationUI();
@@ -129,7 +140,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         updateLocationUI();
     }
 
-    private void showMumbaiAirportLocation(){
+    private void showMumbaiAirportLocation(LatLng current_latlong){
         final Geocoder geocoder = new Geocoder(this, Locale.getDefault());
         bitmapDescriptor
                 = BitmapDescriptorFactory.defaultMarker(
@@ -144,13 +155,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } catch (IOException e) {
             e.printStackTrace();
         }
-           mMap.addMarker(new MarkerOptions().position(mumbai_airport_latlong).icon(bitmapDescriptor).title(title));
-           mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mumbai_airport_latlong, DEFAULT_ZOOM));
-           mMap.getUiSettings().setZoomControlsEnabled(true);
-         //  mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+        mMap.addMarker(new MarkerOptions().position(mumbai_airport_latlong).icon(bitmapDescriptor).title(title));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mumbai_airport_latlong, DEFAULT_ZOOM));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Getting URL to the Google Directions API
+          String url = getUrl(current_latlong, mumbai_airport_latlong);
+          Log.d("onMapClick", url.toString());
+          FetchUrlForMumbai FetchUrl = new FetchUrlForMumbai();
+        // Start downloading json data from Google Directions API
+        FetchUrl.execute(url);
+
     }
 
-    private void showChennaiAirportLocation(){
+    private void showChennaiAirportLocation(LatLng current_latlong){
         final Geocoder geocoder = new Geocoder(this, Locale.getDefault());
         bitmapDescriptor
                 = BitmapDescriptorFactory.defaultMarker(
@@ -168,6 +186,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(chennai_airport_latlong).icon(bitmapDescriptor).title(title));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(chennai_airport_latlong, DEFAULT_ZOOM));
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        // Getting URL to the Google Directions API
+        String url = getUrl(current_latlong, chennai_airport_latlong);
+        Log.d("onMapClick", url.toString());
+        FetchUrlForChennai FetchUrl = new FetchUrlForChennai();
+
+        // Start downloading json data from Google Directions API
+        FetchUrl.execute(url);
 
     }
 
@@ -228,9 +253,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current_latlong, DEFAULT_ZOOM));
                             mMap.getUiSettings().setZoomControlsEnabled(true);
 
-                             // Draw Polyline function from current location to mumbai airport and chennai airport
-                            drawPolylineLocation(current_latitude,current_longitude);
-
+                            // Draw Polyline function from current location to mumbai airport and chennai airport
+                            showMumbaiAirportLocation(current_latlong);
+                            showChennaiAirportLocation(current_latlong);
 
                         } else {
                             LatLng default_latlong = new LatLng(20.5937,78.9629);
@@ -245,20 +270,280 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    private void drawPolylineLocation(double current_latitude,double current_longitude) {
-       mMap.addPolyline(new PolylineOptions()
-                .clickable(true)
-                .add(new LatLng(current_latitude, current_longitude), new LatLng(mumbai_airport_latitude, mumbai_airport_longitude))
-                .width(10)
-                .color(Color.BLACK));
+    private String getUrl(LatLng origin, LatLng dest) {
 
-      mMap.addPolyline(new PolylineOptions()
-                .clickable(true)
-                .add(new LatLng(current_latitude, current_longitude), new LatLng(chennai_airport_latitude, chennai_airport_longitude))
-                .width(10)
-                .color(Color.YELLOW));
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + sensor;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+
+
+        return url;
     }
 
+    // Fetches data from url passed
+    private class FetchUrlForMumbai extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            // For storing data from web service
+            String data = "";
+
+            try {
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+                Log.d("Background Task data", data.toString());
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTaskForMumbai parserTask = new ParserTaskForMumbai();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+
+        }
+    }
+
+    // Fetches data from url passed
+    private class FetchUrlForChennai extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            // For storing data from web service
+            String data = "";
+
+            try {
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+                Log.d("Background Task data", data.toString());
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTaskForChennai parserTask = new ParserTaskForChennai();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+
+        }
+    }
+
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+            Log.d("downloadUrl", data.toString());
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+
+
+    /**
+     * A class to parse the Google Places in JSON format
+     */
+    private class ParserTaskForMumbai extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                Log.d("ParserTask",jsonData[0].toString());
+                DataParser parser = new DataParser();
+                Log.d("ParserTask", parser.toString());
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+                Log.d("ParserTask","Executing routes");
+                Log.d("ParserTask",routes.toString());
+
+            } catch (Exception e) {
+                Log.d("ParserTask",e.toString());
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points;
+            PolylineOptions lineOptions = null;
+
+            // Traversing through all the routes
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList<>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(10);
+                lineOptions.color(Color.RED);
+
+                Log.d("onPostExecute","onPostExecute lineoptions decoded");
+
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            if(lineOptions != null) {
+                mMap.addPolyline(lineOptions);
+            }
+            else {
+                Log.d("onPostExecute","without Polylines drawn");
+            }
+        }
+    }
+
+    /**
+     * A class to parse the Google Places in JSON format
+     */
+    private class ParserTaskForChennai extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                Log.d("ParserTask",jsonData[0].toString());
+                DataParser parser = new DataParser();
+                Log.d("ParserTask", parser.toString());
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+                Log.d("ParserTask","Executing routes");
+                Log.d("ParserTask",routes.toString());
+
+            } catch (Exception e) {
+                Log.d("ParserTask",e.toString());
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points;
+            PolylineOptions lineOptions = null;
+
+            // Traversing through all the routes
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList<>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(10);
+                lineOptions.color(Color.BLUE);
+
+                Log.d("onPostExecute","onPostExecute lineoptions decoded");
+
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            if(lineOptions != null) {
+                mMap.addPolyline(lineOptions);
+            }
+            else {
+                Log.d("onPostExecute","without Polylines drawn");
+            }
+        }
+    }
 
 
 }
